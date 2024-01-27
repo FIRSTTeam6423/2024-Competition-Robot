8747dd9f1bbfe8844b20491f0e4e3d189822cc22
arsamulyak <146568432+arsamulyak@users.noreply.github.com>
2024-01-27 16:48:05 -0500

    Started cargo util. created cargo states

--- src/main/java/frc/robot/Subsystems/CargoUtil.java
+++ src/main/java/frc/robot/Subsystems/CargoUtil.java
@@ -7,6 +7,9 @@ package frc.robot.Subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -16,11 +19,19 @@ import frc.robot.util.IntakeState;
 public class CargoUtil extends SubsystemBase {
   /** Creates a new CargoUtil. */
   private CANSparkMax intakepivotmoter;
-  private IntakeState state=IntakeState.RETRACTED;
-  
+
+  private TrapezoidProfile intakeProfile, ampMechProfile;
+
   public CargoUtil() {
     intakepivotmoter = new CANSparkMax(Constants.INTAKE_PIVOT_MOTOR, MotorType.kBrushless);
 
+
+    
+  }
+
+  public void operateCargo(boolean intake){
+
+    
   }
 
   @Override
