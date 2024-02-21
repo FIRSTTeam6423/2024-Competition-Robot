5c5fbe7fdd7cb8bed4510801f16623f6f79491ef
dabeycorn <dasun20202020@gmail.com>
2024-02-21 09:59:44 -0800

    Added motor ids for climb motors

--- src/main/java/frc/robot/Climb/ClimbConstants.java
+++ src/main/java/frc/robot/Climb/ClimbConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.Climb;
 
 public class ClimbConstants {
+   public static final int CLIMB_LEFT_MOTOR = 0;
+   public static final int CLIMB_RIGHT_MOTOR = 0;
+
    public static final double MAX_EXTEND_VOLTAGE = 6;
    public static final double MAX_RETRACT_VOLTAGE = 12;
 }
