76112f7650755827960c6dba77b279a3b424218a
Team6423 <55815254+Team6423@users.noreply.github.com>
2024-02-22 10:57:00 -0500

    Tuning amp mech

--- src/main/java/frc/robot/AmpMech/AmpMechConstants.java
+++ src/main/java/frc/robot/AmpMech/AmpMechConstants.java
@@ -15,12 +15,12 @@ public final class AmpMechConstants {
     public static final double AMP_MECH_PIVOT_P = 0.025;
     public static final double AMP_MECH_PIVOT_I = 0;
     public static final double AMP_MECH_PIVOT_D = 0;
-    public static final double AMP_MECH_MAX_VELOCITY_DEG_PER_SEC = 600;
-    public static final double AMP_MECH_MAX_ACCELERATION_DEG_PER_SEC_SQUARED = 600;
+    public static final double AMP_MECH_MAX_VELOCITY_DEG_PER_SEC = 2000;
+    public static final double AMP_MECH_MAX_ACCELERATION_DEG_PER_SEC_SQUARED = 2000;
     public static final double AMP_MECH_PIVOT_ENCODER_OFFSET_DEGREES = -50;
     public static final double AMP_MECH_IN_ANGLE = 140;
     public static final double AMP_MECH_ROLLER_SUCK_SPEED = 0.3;
     public static final double AMP_MECH_OUT_ANGLE = 45;
-    public static final double AMP_MECH_DEPOSIT_SPEED = 0.2;
+    public static final double AMP_MECH_DEPOSIT_SPEED = 0.4;
     
 }
